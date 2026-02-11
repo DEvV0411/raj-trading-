6bd6839a3219f3bfadd6330de96b45c641e58655
Dev Gosalia <devgosalia4@gmail.com>
2026-02-11 20:38:03 +0530

    11-02

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import EnquiryModal from './components/EnquiryModal';
@@ -42,6 +42,7 @@ function App() {
             <Route path="/contact" element={<ContactPage />} />
             
             {/* Admin Routes */}
+            <Route path="/admin" element={<Navigate to="/admin/login" replace />} />
             <Route path="/admin/login" element={<AdminLogin />} />
             <Route path="/admin/dashboard" element={<AdminDashboard />} />
           </Routes>
